195b20b45ef2deea21d7ab20600f71657c42944c
amina <amina.1111@yahoo.fr>
2024-01-23 11:03:42 +0100

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:54] + str[106:112] + str[0:6]
+str = str[39:55] + str[-22:-17] + str[0:6]
 print(str)
+
+
+
+
+
