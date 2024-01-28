4af235ad1bd6470a3c5ec81a7ad2b9c3dcea43f0
amina <amina.1111@yahoo.fr>
2024-01-28 14:54:33 +0100

    print number combos

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for n in range(0, 10):
+    for idx in range(0, 10):
+        if n != idx and n*10 + idx < idx*10 + n:
+            if ((n*10 + idx != 89) and (idx*10 + n != 89)):
+                print("{}{}, ".format(n, idx), end="")
+            else:
+                print("{}{}".format(n, idx), end="")
