f7c74394c3cb33c53e497860ed35302d2480c1d2
amina <amina.1111@yahoo.fr>
2024-01-28 20:23:20 +0100

    cmd line arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print(len(sys.argv) - 1, "arguments.")
+    elif len(sys.argv) == 2:
+        print(len(sys.argv) - 1, "argument:")
+    else:
+        print(len(sys.argv) - 1, "arguments:")
+    for idx in range(1, len(sys.argv)):
+        print("{}: {}".format(idx, str(sys.argv[idx])))
