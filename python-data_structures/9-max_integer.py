ce451da3f253b1aa486a6b368dbb0b03a31ba9e7
amina <amina.1111@yahoo.fr>
2024-01-28 23:47:03 +0100

    find max int

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    c = my_list[0]
     if my_list is None:
         return None
     else:
+        c = my_list[0]
         for idx in range(0, len(my_list) - 1):
-            if c < my_list[idx + 1]:
-                c = my_list[idx + 1]
+            if c < my_list[idx]:
+                c = my_list[idx]
             return c
