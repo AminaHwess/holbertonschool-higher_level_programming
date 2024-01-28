d7ac62e1920f71b763ff668a9f3cef06758fd874
amina <amina.1111@yahoo.fr>
2024-01-28 22:28:08 +0100

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    string = ""
     for idx in range(0, len(my_string)):
         if my_string[idx] == 'C' or my_string[idx] == 'c':
-            print('', end="")
-        else:
-            print(my_string[idx], end="")
+            continue
+        string = string + my_string[idx]
+    return string
