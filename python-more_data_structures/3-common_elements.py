25b2b50ab97a51175e479c1fcc989f6745e65281
amina <amina.1111@yahoo.fr>
2024-02-01 18:13:42 +0100

    print common element

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     for element in set_1 & set_2:
-            return element
+            if True:
+                return element
+            else:
+                 continue
