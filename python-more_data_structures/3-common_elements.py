e13850bc38cd1759f1010835379e394bc2ca61f9
amina <amina.1111@yahoo.fr>
2024-02-01 18:21:12 +0100

    print common element

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for element in set_1 & set_2:
-        if element:
-            return element
-        else:
-            continue
+    inter = set_1.intersection(set_2)
+    return inter
