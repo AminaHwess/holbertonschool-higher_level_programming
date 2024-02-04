dc3827b65dc38737a4d84e382aaacefd3b7ab68c
amina <amina.1111@yahoo.fr>
2024-02-04 21:29:06 +0100

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+Empty Square with private instance 'size'
+"""
+
+
 class Square:
+    """
+    Empty square.
+    """
+
     def __init__(self, size):
+        """
+        Empty Square with private instance 'size'.
+        """
         self._Square__size = size
         pass
