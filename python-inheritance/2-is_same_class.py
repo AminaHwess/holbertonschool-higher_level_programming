e244f53f5dbf11dd8fd10f022a96c37885787f59
amina <amina.1111@yahoo.fr>
2024-02-08 19:02:20 +0100

    2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+check if object is instance of class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    check if object is instance of class
+    """
+    if type(obj) is a_class:
+        return True
+    return False
