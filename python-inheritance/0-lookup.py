450e30a67f8b78d3f502493242fe65b7cc04690f
amina <amina.1111@yahoo.fr>
2024-02-08 17:20:23 +0100

    0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,6 +6,6 @@ return methods of object
 
 def lookup(obj):
     """
-    function that returns the list of available attributes and methods of an object
+    returns list of available attributes and methods of an object
     """
     return dir(obj)
