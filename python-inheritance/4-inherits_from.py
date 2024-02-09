232e31ab7f5d1c3b197633c10396c66de3b5d4a2
amina <amina.1111@yahoo.fr>
2024-02-09 10:39:48 +0100

    4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+check if object is instance of class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    check if object is instance of class
+    """
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
