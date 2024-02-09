e2b2e2d926f94c07ba14f276bc8fc87f20bf47be
amina <amina.1111@yahoo.fr>
2024-02-09 10:57:37 +0100

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""
+raise exception
+"""
+
+
+class BaseGeometry:
+    """
+    raise exception
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
