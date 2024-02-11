9be2b6ef48594b2df28d8fb3ed858a6fe8391c0c
amina <amina.1111@yahoo.fr>
2024-02-11 13:36:38 +0100

    10-square.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Class Square that inherits from Rectangle
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square that inherits from Rectangle
+    """
+
+    def __init__(self, size):
+        super().__init__(size, size)
