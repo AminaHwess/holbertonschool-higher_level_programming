dec484e9b56b2638dfcaa1df3b7d684f6431eeac
amina <amina.1111@yahoo.fr>
2024-02-11 13:40:23 +0100

    10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -13,5 +13,4 @@ class Square(Rectangle):
     def __init__(self, size):
         self.__size = size
         self.integer_validator("size", size)
-        self.area(self)
         super().__init__(size, size)
