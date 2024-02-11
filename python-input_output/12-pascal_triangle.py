887982341eb9a35e484063e394b53712be9ba909
amina <amina.1111@yahoo.fr>
2024-02-11 23:45:33 +0100

    12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+print pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    print pascal's triangle
+    """
+    if n <= 0:
+        return []
+    triangle = [[]]
+    for i in range(1, n):
+        previous = triangle[-1]
+        first = [1]
+        for j in range(1, i):
+            first.append(previous[j - 1] + previous[j])
+
+        first.append(1)
+        triangle.append(first)
+
+    return triangle
