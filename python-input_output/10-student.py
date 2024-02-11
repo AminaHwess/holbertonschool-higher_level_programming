58c2a384c3c89b33972dc52dcd1e6445c5a7475d
amina <amina.1111@yahoo.fr>
2024-02-11 22:39:35 +0100

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Class 'Student'
+"""
+
+
+class Student:
+    """
+    Retrieve dict representation of Student instance
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(element) is str for element in attrs):
+            return {
+                element: getattr(self, element)
+                for element in attrs
+                if hasattr(self, element)
+            }
+        return self.__dict__
