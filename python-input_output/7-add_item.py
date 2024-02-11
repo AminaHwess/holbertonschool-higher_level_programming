9d6bae0cdafb15ac6dbe7d149437a8f6bff8c00a
amina <amina.1111@yahoo.fr>
2024-02-11 18:56:22 +0100

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+add new argument to python list
+"""
+
+import json
+import sys
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+newlist = []
+for i in range(1, len(sys.argv)):
+    newlist.append(sys.argv[i])
+    save_to_json_file(newlist, filename)
+    load_from_json_file(filename)
