1c34c7ce46f564c7f5f507f3cf1185e3276706a1
amina <amina.1111@yahoo.fr>
2024-02-11 21:43:26 +0100

    8-class_to_json.py

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+turn class obj into JSON str rep
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    turn class obj into JSON string rep
+    """
+
+    return json.dumps(obj.__dict__)
