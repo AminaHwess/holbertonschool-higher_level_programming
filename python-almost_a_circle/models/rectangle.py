ef918f20b52b87bd9d6a98d8227e4a2af9cf02fa
amina <amina.1111@yahoo.fr>
2024-02-14 10:58:51 +0100

    Create class : Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,8 +15,8 @@ class Rectangle(base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.height = height
         self.width = width
+        self.height = height
         self.x = x
         self.y = y
 
