db90650b5b3241157f24a1e6c05bcc2d5eacd1be
amina <amina.1111@yahoo.fr>
2024-02-14 09:35:01 +0100

    Create class : Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -42,20 +42,32 @@ class Rectangle(base):
 
         @width.setter
         def width(self, value):
-            if isinstance(value, int) and value >= 0:
-                self.__width = value
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
 
         @height.setter
         def height(self, value):
-            if isinstance(value, int) and value >= 0:
-                self.__height = value
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
 
         @x.setter
         def X(self, value):
-            if isinstance(value, int) and value >= 0:
-                self.__x = value
+            if not isinstance(value, int):
+                raise TypeError("x must be an integer")
+            if value < 0:
+                raise ValueError("x must be >= 0")
+            self.__x = value
 
         @y.setter
-        def X(self, value):
-            if isinstance(value, int) and value >= 0:
-                self.__y = value
+        def Y(self, value):
+            if not isinstance(value, int):
+                raise TypeError("y must be an integer")
+            if value < 0:
+                raise ValueError("y must be >= 0")
+            self.__y = value
