c9b50f3a94a60cf44cfe191d71928cbe7eb71885
amina <amina.1111@yahoo.fr>
2024-02-14 09:30:52 +0100

    Create class : Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -36,16 +36,26 @@ class Rectangle(base):
         def width(self):
             return self.__width
 
+        @property
+        def height(self):
+            return self.__height
+
         @width.setter
         def width(self, value):
             if isinstance(value, int) and value >= 0:
                 self.__width = value
 
-        @property
-        def height(self):
-            return self.__height
-
         @height.setter
         def height(self, value):
             if isinstance(value, int) and value >= 0:
                 self.__height = value
+
+        @x.setter
+        def X(self, value):
+            if isinstance(value, int) and value >= 0:
+                self.__x = value
+
+        @y.setter
+        def X(self, value):
+            if isinstance(value, int) and value >= 0:
+                self.__y = value
