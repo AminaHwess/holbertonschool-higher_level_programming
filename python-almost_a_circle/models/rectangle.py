c08512e1dc3096c0b4c2f297fd9f647d04df26a9
amina <amina.1111@yahoo.fr>
2024-02-14 12:38:24 +0100

    testing

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" module"""
+""" module documentation """
 
 from models.base import Base
 
 class Rectangle(Base):
     """Class Rectangle """
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """ constructor of the class """
         super().__init__(id)
