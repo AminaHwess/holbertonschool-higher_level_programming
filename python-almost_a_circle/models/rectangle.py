89a2eb09b2e2ec6ab2e0e6b5e914d2b019018db5
amina <amina.1111@yahoo.fr>
2024-02-14 12:58:09 +0100

    Creating Class: Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,11 +3,12 @@
 
 from models.base import Base
 
+
 class Rectangle(Base):
-    """Class Rectangle """
+    """Class Rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ constructor of the class """
+        """constructor of the class"""
         super().__init__(id)
         self.width = width
         self.height = height
