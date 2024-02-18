ba3ba6a5d66ec3d7bc536bf8a2055fd76fd17609
amina <amina.1111@yahoo.fr>
2024-02-18 18:02:28 +0100

    returns an instance with all attributes already set

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -61,11 +61,7 @@ class Base:
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
         from models.rectangle import Rectangle
-        from models.square import Square
 
-        if cls is Rectangle:
-            obj = Rectangle(1, 2, 3, 4, 5)
-        if cls is Square:
-            obj = Square(1, 2, 3, 4)
+        obj = Rectangle(1, 2, 3, 4, 5)
         obj.update(**dictionary)
         return obj
