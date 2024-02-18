acde1b77d47a2ffbf250dd9c6c10d9fac521ae44
amina <amina.1111@yahoo.fr>
2024-02-18 18:16:01 +0100

    returns an instance with all attributes already set

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -66,6 +66,8 @@ class Base:
             obj = Rectangle(1, 2, 3, 4, 5)
         if cls is Square:
             obj = Square(1, 2, 3, 4)
+        if cls is None:
+            obj = None
         obj.update(**dictionary)
         return obj
 
