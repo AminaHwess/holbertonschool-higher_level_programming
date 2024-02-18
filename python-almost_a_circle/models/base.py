dc376904e14d73c8025fca5b218cc09d16068e3c
amina <amina.1111@yahoo.fr>
2024-02-18 13:09:07 +0100

    return JSON string representation of a list of dictionaries

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Creating a first class : base.
 """
+import json
 
 
 class Base:
@@ -23,3 +24,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Return JSON string representation of a list of dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
