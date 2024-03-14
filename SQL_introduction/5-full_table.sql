1580281e2062758c6b08e03fafbd4fe0db40c472
amina <amina.1111@yahoo.fr>
2024-03-14 14:43:38 +0100

    print a description of table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- a script that prints a description of the table from the database
-SHOW COLUMNS FROM first_table;
+-- a script that prints a description of the table
+SHOW COLUMNS FROM first_table FROM hbtn_0c_0;
