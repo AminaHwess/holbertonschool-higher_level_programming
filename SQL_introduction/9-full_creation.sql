fc7b6145cc83a758408c1c1860a6508e63b3c337
amina <amina.1111@yahoo.fr>
2024-03-15 15:52:39 +0100

    second table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
--- a script that creates a table called second_table in the current database in your MySQL server.
+-- a script that creates a table called in the current database.
 CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
 );
-INSERT INTO first_table (id, name, score)
-VALUES  (1, 'John', 10),
-        (2, 'Alex', 3),
-        (3, 'Bob', 14), 
-        (4, 'George', 8);
+INSERT INTO second_table (id, name, score)
+VALUES  ('1', 'John', '10'),
+        ('2', 'Alex', '3'),
+        ('3', 'Bob', '14'), 
+        ('4', 'George', '8');
