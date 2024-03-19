9be3c87ac7ec9cff2c24216320cef27345b0b262
amina <amina.1111@yahoo.fr>
2024-03-19 12:40:21 +0100

    create db and table cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- create db and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+CREATE TABLE IF NOT EXISTS cities(
     id INT NOT NULL UNIQUE AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
