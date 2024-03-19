1d07ad39609e7f945209b1da8963d188664cd8d3
amina <amina.1111@yahoo.fr>
2024-03-19 12:41:45 +0100

    create db and table cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS cities(
     id INT NOT NULL UNIQUE AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
     PRIMARY KEY (id)
 );
