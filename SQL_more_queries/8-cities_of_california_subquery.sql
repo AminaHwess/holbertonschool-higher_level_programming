56ea5088ba9d417c896350e6808b1a17b7f56893
amina <amina.1111@yahoo.fr>
2024-03-21 11:10:38 +0100

    script that lists cities of California in the db hbtn_0d_usa

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+-- script that lists cities of California in the db hbtn_0d_usa
+SELECT * FROM hbtn_0d_usa.cities WHERE state_id IN (SELECT id FROM hbtn_0d_usa.states WHERE name = "California") ORDER BY cities.id ASC;
