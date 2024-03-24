1d10d0d271d8b7befe50cebff5e1cd215db27372
amina <amina.1111@yahoo.fr>
2024-03-24 12:58:58 +0100

    a script that lists all states from the database hbtn_0e_0_usa

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+db=MySQLdb.connect(host="localhost",port = 3306, user=sys.argv[1], 
+                passwd=sys.argv[2], db=sys.argv[3])
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY states.id")
+states = cur.fetchall()
+for state in states:
+    print(state)
+cur.close()
+db.close()
