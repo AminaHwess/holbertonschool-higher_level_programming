052e596537d28a41391162e1b0ec2d698ea041f4
amina <amina.1111@yahoo.fr>
2024-05-28 11:40:38 +0100

    1-script.js

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-document.getElementById('red_header').addEventListener('click', event => document.querySelector('header').style.color = '#FF0000');
+document.getElementById('red_header').addEventListener('click', () => document.querySelector('header').style.color = '#FF0000');
